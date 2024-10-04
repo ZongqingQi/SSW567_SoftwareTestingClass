4b0a9b32111e67b3b13843a978dfad52f5ff3c6b
ZongqingQi <zongqingqi@gmail.com>
2024-10-04 15:59:11 -0400

    HW02a Fix bugs in Triangle

--- HW02a/Triangle.py
+++ HW02a/Triangle.py
@@ -9,6 +9,16 @@ The primary goal of this file is to demonstrate a simple python program to class
 @author: rk
 """
 
+def isRightTriangle(a,b,c):
+    a_sq = round(a**2, 4)
+    b_sq = round(b**2, 4)
+    c_sq = round(c**2, 4)
+
+    if (round((a_sq + b_sq), 4) == c_sq) or (round((a_sq + c_sq), 4) == b_sq) or (round((c_sq + b_sq), 4) == a_sq):
+        return True
+    else:
+        return False
+
 def classifyTriangle(a,b,c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of 
@@ -27,31 +37,35 @@ def classifyTriangle(a,b,c):
       BEWARE: there may be a bug or two in this code
     """
 
+    # verify that all 3 inputs are integers or float
+    # Python's "isinstance(object,type) returns True if the object is of the specified type
+    if not( (isinstance(a,int) or isinstance(a,float)) and (isinstance(b,int) or isinstance(b,float)) and (isinstance(c,int) or isinstance(c,float)) ):
+        return 'InvalidInput'
+
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
         
-    if a <= 0 or b <= b or c <= 0:
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
     
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
-      
     # This information was not in the requirements spec but 
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    if (a+b <= c) or (a+c <= b ) or (b+c <= a):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
-    if a == b and b == a:
+    if a == b and b == c and c == a:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
-        return 'Right'
+    elif (a==b) or (a==c) or (b==c):
+        if isRightTriangle(a,b,c):
+            return 'Isoceles and Right'
+        else:
+            return 'Isoceles'
     elif (a != b) and  (b != c) and (a != b):
-        return 'Scalene'
-    else:
-        return 'Isoceles'
+        if isRightTriangle(a,b,c):
+            return 'Right'
+        else:
+            return 'Scalene'
